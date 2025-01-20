c468b3c8d2ae1f4b0797131f35af4f22eb6eda43
madrimov7611 <javohirmadrimov35@gmail.com>
2025-01-20 15:51:02 +0500

    chslasi bor

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.utils import timezone
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
@@ -20,12 +22,24 @@ class PublishedManager(models.Manager):
 
 
 class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
     title = models.CharField(max_length=255)
     category = models.ForeignKey(to=Category, on_delete=models.CASCADE)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
     slug = models.SlugField(max_length=200, unique=True)
     image = models.ImageField(upload_to='rasmlar/')
     description = models.TextField()
     create_ad = models.DateTimeField(auto_now_add=True)
+    publish = models.DateTimeField(default=timezone.now, blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft',
+    blank=True, null=True)
+    objects = models.Manager()
+    published = PublishedManager()
 
     def __str__(self):
         return self.title
